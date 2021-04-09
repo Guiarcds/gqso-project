e657cc992c5afaaff2ef9ec5d9847caa44dde93a
Guilherme Augusto <guilherme.cavalcante78@gmail.com>
2021-04-09 15:30:20 -0300

    Att class TestBank with setUp method

--- Bank/src/test/java/bank/TestBank.java
+++ Bank/src/test/java/bank/TestBank.java
@@ -1,5 +1,14 @@
 package bank;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class TestBank{
-    
+    Bank bank;
+
+    @BeforeEach
+    public void setUp(){
+        bank = new Bank();
+    }
 }
