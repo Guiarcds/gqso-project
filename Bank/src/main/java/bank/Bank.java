e8dc5a524c0e5098d7a0ad543beaa0844b9853f0
Guilherme Augusto <guilherme.cavalcante78@gmail.com>
2021-04-09 22:27:46 -0300

    Add method saque and insert a failed test

--- Bank/src/main/java/bank/Bank.java
+++ Bank/src/main/java/bank/Bank.java
@@ -1,13 +1,18 @@
 package bank;
 
-public class Bank{
+public class Bank {
     private double saldo;
 
-    public double saldo(){
+    public double saldo() {
         return this.saldo;
     }
 
-    public void setSaldo(double value){
+    public void setSaldo(double value) {
         this.saldo = value;
+    }
+
+    public double saque(double value){
+        setSaldo(saldo() - value);
+        return saldo();
     }
 }
--- Bank/src/test/java/bank/TestBank.java
+++ Bank/src/test/java/bank/TestBank.java
@@ -4,17 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class TestBank{
+public class TestBank {
     Bank bank;
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         bank = new Bank();
         bank.setSaldo(50);
     }
 
     @Test
-    public void saqueTest (){
-
+    public void saqueTest() {
+        assertEquals(bank.saldo() - 51, bank.saque(50));
     }
 }
