3a832afe6f80f9eed46813df96f971ec9f5915dd
Guilherme Augusto <guilherme.cavalcante78@gmail.com>
2021-04-14 18:34:13 -0300

    add main class

---
+++ Bank/src/main/java/bank/Main.java
@@ -0,0 +1,38 @@
+package bank;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Bank bk = new Bank();
+        Scanner in = new Scanner(System.in);
+
+        for (;;) {
+            System.out.println("| 1) Saldo     |\n| 2) Saque     |\n| 3) Depósito  |\n| 0)  Sair     |");
+            int operation = in.nextInt();
+            double value;
+
+            if (operation == 1) {
+                System.out.println("Seu Saldo é de: $" + bk.saldo() + "\n");
+            }
+            if (operation == 2) {
+                System.out.println("Digite o valor do Saque:\n");
+                value = in.nextDouble();
+                bk.saque(value);
+            }
+            if (operation == 3) {
+                System.out.println("Digite o valor do Depósito:\n");
+                value = in.nextDouble();
+                bk.deposito(value);
+            }
+            if (operation == 0) {
+                System.out.println("Até a próxima!");
+                break;
+            } else {
+                System.out.println("Operação Desconhecida!!!");
+            }
+
+        }
+
+    }
+}
